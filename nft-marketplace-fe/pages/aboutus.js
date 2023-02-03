e0f01f83f183f43c22346ca8cffe912ee14b7168
adhyaksazhalifunnas <adhyaksazhalifunnas@gmail.com>
2023-02-03 15:53:22 +0700

    fix(brand): minor hardcode changes

--- nft-marketplace-fe/pages/aboutus.js
+++ nft-marketplace-fe/pages/aboutus.js
@@ -32,16 +32,16 @@ const aboutus = () => {
 
     const factsArray = [
         {
-            title: "10 million",
-            info: "Articles have been public around the world (as of Sept. 30, 2021)",
+            title: "N/A",
+            info: "Articles have been public around the world (as of 2/2/2023)",
         },
         {
-            title: "100,000",
-            info: "Registered users account (as of Sept. 30, 2021)",
+            title: "N/A",
+            info: "Registered users account (as of 2/2/2023)",
         },
         {
-            title: "220+",
-            info: "Countries and regions have our presence (as of Sept. 30, 2021",
+            title: "N/A",
+            info: "Countries and regions have our presence (as of 2/2/2023)",
         },
     ];
     return (
@@ -49,11 +49,11 @@ const aboutus = () => {
             <div className={Style.aboutus_box}>
                 <div className={Style.aboutus_box_hero}>
                     <div className={Style.aboutus_box_hero_left}>
-                        <h1>👋 About Us.</h1>
+                        <h1>👋 About Us</h1>
                         <p>
-                            We're impartial and independent, and every day we create
-                            distinctive, world-class programmes and content which inform,
-                            educate and entertain millions of people in the around the world.
+                            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                            Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+                            Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
                         </p>
                     </div>
                     <div className={Style.aboutus_box_hero_right}>
@@ -64,8 +64,8 @@ const aboutus = () => {
                 <div className={Style.aboutus_box_title}>
                     <h2>⛱ Founder</h2>
                     <p>
-                        We're impartial and independent, and every day we create
-                        distinctive, world-class programmes and content
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+                        sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
                     </p>
                 </div>
 
@@ -90,8 +90,8 @@ const aboutus = () => {
                 <div className={Style.aboutus_box_title}>
                     <h2>🚀 Fast Facts</h2>
                     <p>
-                        We're impartial and independent, and every day we create
-                        distinctive, world-class programmes and content
+                        Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+                        sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
                     </p>
                 </div>
 
